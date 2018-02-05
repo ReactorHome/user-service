aa7cc8b44f1b05567c32dbdb2c90cadd58c39603
Michael Smith <mdsf3f@mail.missouri.edu>
2018-02-05 14:34:08 -0600

    Account and group updates

--- src/main/java/reactor/controllers/AccountController.java
+++ src/main/java/reactor/controllers/AccountController.java
@@ -3,20 +3,27 @@ package reactor.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.exceptions.UsernameAlreadyExists;
 import reactor.models.Account;
+import reactor.models.Group;
+import reactor.models.User;
 import reactor.repositories.AccountRepository;
+import reactor.repositories.GroupRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/users")
 public class AccountController {
 
     private AccountRepository accountRepository;
+
+    @Autowired
+    private GroupRepository groupRepository;
 
     @Autowired
     PasswordEncoder passwordEncoder;
@@ -33,5 +40,18 @@ public class AccountController {
         account.setPassword(passwordEncoder.encode(account.getPassword()));
         accountRepository.save(account);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("me/groups")
+    public List<Group> groups(@AuthenticationPrincipal User user){
+        List<Group> groups = new ArrayList<>(groupRepository.findByOwnerIs(user.account));
+        groups.addAll(groupRepository.findByAccountListIs(user.account));
+
+        return groups;
+    }
+
+    @GetMapping("me")
+    public User me(@AuthenticationPrincipal User user){
+        return user;
     }
 }
--- src/main/java/reactor/repositories/GroupRepository.java
+++ src/main/java/reactor/repositories/GroupRepository.java
@@ -2,10 +2,14 @@ package reactor.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import reactor.models.Account;
 import reactor.models.Group;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GroupRepository extends CrudRepository<Group, Integer> {
     Optional<Group> findById(Integer id);
+    List<Group> findByOwnerIs(Account owner);
+    List<Group> findByAccountListIs(Account account);
 }
